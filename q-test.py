2892f3490c620a7009d9db0e57ca232250def901
Umut CAN <78921017+U-C4N@users.noreply.github.com>
2025-09-11 17:56:25 +0300

    v2

---
+++ q-test.py
@@ -0,0 +1,17 @@
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
+from ai_translator import AITranscriptTranslator
+
+VIDEO_ID = " "  # Test için video ID
+API_KEY = " " # API Anahtarın
+OUTPUT_FILE = "translation_output.txt" # Çıktı dosyasının adı
+
+try:
+    translator = AITranscriptTranslator(API_KEY)
+    translation = translator.set_lang("English").set_type("txt").translate_transcript(VIDEO_ID)
+    with open(OUTPUT_FILE, "w", encoding="utf-8") as f:
+        f.write(translation)
+    print(f"✅ Çeviri tamamlandı ve '{OUTPUT_FILE}' dosyasına kaydedildi.")
+except Exception as e:
+    print(f"❌ Hata: {e}") 
